6d24c331efa7fd3bbad4141ed5a3526195813ffb
DavyVR1987 <davyvanroy@yahoo.com>
2021-05-18 10:50:17 +0200

    Initial project load

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,12 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/movies",
+    name: "Movies",
+    component: () => 
+      import("../views/Movies.vue"),
+  }
 ];
 
 const router = createRouter({
